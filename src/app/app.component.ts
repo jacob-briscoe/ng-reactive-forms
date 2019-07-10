e25af873f28c72266354c5df941c8eee254f8a61
Jacob Briscoe <jacob.briscoe@adesa.com>
2019-07-10 15:53:48 -0400

    Initial Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-reactive-forms';
+  form: FormGroup;
+  fullName = new FormControl("", Validators.required);
+
+  constructor(fb: FormBuilder){
+    this.form = fb.group({
+      'fullName': this.fullName,
+      'password': ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('Form was submitted!');
+    console.log(this.form);
+  }
 }
